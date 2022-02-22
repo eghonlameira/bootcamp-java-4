7975dbbc700faf655d4e55f2ae7a6b97ec4fd5eb
Eghon <eghon.fernandes@ee.ufcg.edu.br>
2022-02-21 22:18:15 -0300

    atualizacao do repositorio

---
+++ Projetos Java/src/com/dio/FirstProgram.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a;
+        int b;
+        System.out.println("Hello World! I'm little guy in a big game." + (a+b));*/
+    }
+}
